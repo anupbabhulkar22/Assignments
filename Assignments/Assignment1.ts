bdc704299bbea546667c76a6255a15f2f59a4fa3
anupbabhulkar22 <anupbabhulkar22@gmail.com>
2026-02-11 20:33:12 +0530

    Assignments1

---
+++ Assignments/Assignment1.ts
@@ -0,0 +1,79 @@
+// 1. Temperature of a city in degrees Celsius
+let cityTemp: number = 25.5;
+console.log("Temperature of the city in degree celsius:", cityTemp);
+
+// 2. Whether a customer has placed an order
+let custOrder: boolean = false;
+console.log("Has the customer placed an order", custOrder);
+
+// 3. Person's phone number
+let phNumber: string = "777-786-7849";
+console.log("Person's phone number:", phNumber);
+
+// 4. Amount of money in a customer's bank account
+let AccBalance: number = 7845.33;
+console.log("Customer's bank account balance:", AccBalance);
+
+// 5. Person's email address
+let emailAddress: string = "anupb@vscode.com";
+console.log("Person's email address:", emailAddress);
+
+// 6. Coordinates of a location (latitude, longitude)
+let locationCoordinates: [number, number] = [37.7749, -122.4194];
+console.log("Coordinates of a location - Latitude:", locationCoordinates[0], ", Longitude:", locationCoordinates[1]);
+
+// 7. Person's marital status
+let Married: boolean = false;
+console.log("Person's marital status", Married);
+
+// 8. Person's occupation
+let occupation: string = "Software Engineer";
+console.log("Person's occupation:", occupation);
+
+// 9. Person's favourite colour
+let favouriteColour: string = "Grey";
+console.log("Person's favourite colour", favouriteColour);
+
+// 10. Current year
+let currentYear: number = 2026;
+console.log("Current year:", currentYear);
+
+// 11. Number of followers on a social media platform
+let followersCnt: number = 1000000;
+console.log("Number of followers:", followersCnt);
+
+// 12. Rating of a movie
+let movieRating: number = 8.5;
+console.log("Movie rating:", movieRating);
+
+// 13. Person's blood type
+let bloodType: string = "A+";
+console.log("Person's blood type:", bloodType);
+
+// 14. Title of a book
+let bookTitle: string = "The 3 Mistakes of My Life";
+console.log("Book title:", bookTitle);
+
+// 15. Number of employees in a company
+let employeesCount: number = 500;
+console.log("Number of employees in the company:", employeesCount);
+
+// 16. Time of an event
+let eventTime: string = "7:30 PM";
+console.log("Time of the event:", eventTime);
+
+// 17. Name of a country
+let countryName: string = "INDIA";
+console.log("Name of the country:", countryName);
+
+// 18. Person's eye color
+let eyeColor: string = "Brown";
+console.log("Person's eye color:", eyeColor);
+
+// 19. Person's birthplace
+let birthplace: string = "Nagpur";
+console.log("Person's birthplace:", birthplace);
+
+// 20. Distance between two cities
+let distanceBetweenCities: number = 720.5;
+console.log("Distance between two cities (km):", distanceBetweenCities);
